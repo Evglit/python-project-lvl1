e3c13656bf1b36e7f430608241258145c65318c8
Evgeny <litun-07@mail.ru>
2020-11-25 23:08:43 +0300

    add step 5

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,5 +5,7 @@ import prompt
 
 def welcome_user():
     """Get an user name and promt user."""
+    print('Welcome to the Brain Games!\n')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
+    return name
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Check even."""
+
+from random import randint
+
+import prompt
+
+from brain_games import cli
+
+
+def check_even(name_user):
+    """Funtion for check even."""
+    need_answers = 3
+    sum_answers = 0
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while sum_answers < need_answers:
+        number = randint(0, 100)
+        print('Question: {}'.format(number))
+        if number == 0:
+            right_answer = 'no'
+        elif number % 2 == 0:
+            right_answer = 'yes'
+        else:
+            right_answer = 'no'
+        player_answer = prompt.string('Your answer: ')
+        if player_answer == right_answer:
+            sum_answers += 1
+            print('Correct!')
+            if sum_answers == need_answers:
+                print('Congratulations, {}!'.format(name_user))
+        else:
+            print(
+                "'{}' is wrong answer ;(. ".format(player_answer)
+                +
+                "Correct answer was '{}'.".format(right_answer)
+                )
+            print("Let's try again, {}!".format(name_user))
+            break
+
+
+def main():
+    """Make a user intreface."""
+    check_even(cli.welcome_user())
+
+
+if __name__ == '__main__':
+    main()
